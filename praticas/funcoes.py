54687620527fe7a6584631763d63490d670e268b
Ewerton <68349173+ewerton3000@users.noreply.github.com>
2026-06-03 17:47:50 -0300

    Primeiros algoritmos do python
    Primeiros algoritmos do python como variaveis , if e else , input e outros testes

---
+++ praticas/funcoes.py
@@ -0,0 +1,6 @@
+def boas_vindas():
+    print("fala dev! Beleza?")
+    print("Vamos programar hoje")
+
+    boas_vindas()
+    
---
+++ praticas/numeros.py
@@ -0,0 +1,7 @@
+idade = 4
+if idade>= 18:
+ print("maior idade")
+if idade== 35:
+ print("Esse cara sou eu")
+else:
+  print("menor de idade")
